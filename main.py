124a32b5e6a7228d0ed8179165589ce86704298a
Bigimus <67165421+Bigimus@users.noreply.github.com>
2023-01-07 20:12:49 -0500

    Update main.py
    Fixed spelling mistakes.

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ root.geometry("200x300")
 
 def dinnerMenu():
     temp_dinner = functions.getDinner()
-    messagebox.showinfo(title = "Dinner Resturant", message = temp_dinner)
+    messagebox.showinfo(title = "Dinner Restaurant", message = temp_dinner)
     
-dinnerButton = tk.Button(root, text = "Random Dinner Resturant", command = lambda:[functions.setDinner(), dinnerMenu()])
+dinnerButton = tk.Button(root, text = "Random Dinner Restaurant", command = lambda:[functions.setDinner(), dinnerMenu()])
 dinnerButton.pack()
 
 def breakfastMenu():
     temp_breakfast = functions.getBreakfast()
-    messagebox.showinfo(title = "Breakfast Resturant", message = temp_breakfast)
+    messagebox.showinfo(title = "Breakfast Restaurant", message = temp_breakfast)
     
-breakfastButton = tk.Button(root, text = "Random Breakfast Resturant", command = lambda:[functions.setBreakfast(), breakfastMenu()])
+breakfastButton = tk.Button(root, text = "Random Breakfast Restaurant", command = lambda:[functions.setBreakfast(), breakfastMenu()])
 breakfastButton.pack()
 
 def desertMenu():
     temp_desert = functions.getDesert()
-    messagebox.showinfo(title = "Desert Resturant", message = temp_desert)
+    messagebox.showinfo(title = "Desert Restaurant", message = temp_desert)
     
-dinnerButton = tk.Button(root, text = "Random Desert Resturant", command = lambda:[functions.setDesert(), desertMenu()])
+dinnerButton = tk.Button(root, text = "Random Desert Restaurant", command = lambda:[functions.setDesert(), desertMenu()])
 dinnerButton.pack()
 
 root.mainloop()
